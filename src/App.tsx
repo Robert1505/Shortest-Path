532241573eea54993ebebc4ba0cff0dfec790659
Patainea Robert <patainearobert@yahoo.com>
2021-11-06 11:06:12 +0200

    created grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+const ROWS = 22;
+const COLUMNS = 61;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [matrix, setMatrix] = useState(
+    Array(ROWS).fill(Array(COLUMNS).fill(0))
   );
+
+  const renderGrid = () => {
+    return matrix.map((row: any, rowIndex: number) => {
+      return (
+        <div className="grid" key={`row-${rowIndex}`}>
+          {row.map((cell: number) => {
+            return <div></div>;
+          })}
+        </div>
+      );
+    });
+  };
+
+  return <div className="App">{renderGrid()}</div>;
 }
 
 export default App;
