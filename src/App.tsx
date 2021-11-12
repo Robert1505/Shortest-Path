d1c990a8310d6b597380564706651fa73b6bd36b
Patainea Robert <patainearobert@yahoo.com>
2021-11-12 16:04:44 +0200

    path1

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
+import Cell from "./Cell";
 
 const ROWS = 22;
 const COLUMNS = 61;
 
-const START_X = 0;
+const START_X = 12;
 const START_Y = 0;
 
 const END_X = 10;
 const END_Y = 10;
 
-enum CELL_TYPE {
+export enum CELL_TYPE {
   EMPTY = 0,
   START = 1,
   END = 2,
@@ -19,38 +20,63 @@ enum CELL_TYPE {
 
 function App() {
   const [matrix, setMatrix] = useState<CELL_TYPE[][]>([]);
+  const [pathMatrix, setPathMatrix] = useState<boolean[][]>([]);
 
   useEffect(() => {
     const mat = [];
+    const pMat = [];
     for (let i = 1; i <= ROWS; i++) {
       const row = [];
+      const pRow = [];
       for (let j = 0; j < COLUMNS; j++) {
         row.push(CELL_TYPE.EMPTY);
+        pRow.push(false);
       }
       mat.push(row);
+      pMat.push(pRow);
     }
     mat[START_X][START_Y] = CELL_TYPE.START;
     mat[END_X][END_Y] = CELL_TYPE.END;
 
+    pMat[START_X][START_Y] = true;
+
+    if (START_Y > END_Y) {
+      for (let i = END_Y; i < START_Y; i++) pMat[START_X][i] = true;
+    } else {
+      for (let i = START_Y; i <= END_Y; i++) pMat[START_X][i] = true;
+    }
+
+    if(START_X > END_X){
+      for (let i = END_X; i < START_X; i++) pMat[i][END_Y] = true;
+    } else {
+      for (let i = START_X; i <= END_X; i++) pMat[i][END_Y] = true;
+    }
+
     setMatrix(mat);
+    setPathMatrix(pMat);
   }, []);
 
   const renderGrid = () => {
     return matrix.map((row: any, rowIndex: number) => {
       return (
         <div className="grid" key={`row-${rowIndex}`}>
-          {row.map((cell: CELL_TYPE) => {
-            if (cell === CELL_TYPE.EMPTY) return <div></div>;
-            if (cell === CELL_TYPE.START) return <div><i className="fas fa-chevron-right"></i></div>;
-            if (cell === CELL_TYPE.END) return <div><i className="fas fa-bullseye"></i></div>;
-            if (cell === CELL_TYPE.WALL) return <div><i className="fas fa-tree"></i></div>;
-          })}
+          {row.map((cellType: CELL_TYPE, columnIndex: number) => (
+            <Cell
+              cellType={cellType}
+              isYellow={pathMatrix?.[rowIndex]?.[columnIndex]}
+            />
+          ))}
         </div>
       );
     });
   };
 
-  return <div className="App">{renderGrid()}</div>;
+  return (
+    <div className="App">
+      {renderGrid()}
+      <button>START</button>
+    </div>
+  );
 }
 
 export default App;
