9c847a9f03ab3231674a2964bf42fbcf3a78f5db
Patainea Robert <patainearobert@yahoo.com>
2021-11-19 16:12:58 +0200

    Finished DFS Path

--- src/Helpers/floodFillHelper.tsx
+++ src/Helpers/floodFillHelper.tsx
@@ -1,9 +1,12 @@
 import { CELL_TYPE, COLUMNS, dx, dy, ROWS } from "../App";
+import { Move } from "../Types";
+import { END_X, END_Y } from "../App";
 
 export const floodFillRecursiveHelper = (
   squares: boolean[][],
   i: number,
-  j: number
+  j: number,
+  moveHistory: Move[]
 ) => {
   // check out of bounds
   if (i < 0 || i >= ROWS) return 0;
@@ -11,14 +14,15 @@ export const floodFillRecursiveHelper = (
 
   // check if it's visited
   if (squares[i][j]) return 0;
+  if (squares[END_X][END_Y]) return 0;
   // Indicate node has been visited
   squares[i][j] = true;
+  moveHistory.push({ positionX: i, positionY: j });
 
   for (let a = 0; a < 4; a++) {
-    floodFillRecursiveHelper(squares, i + dx[a], j + dy[a]);
+    floodFillRecursiveHelper(squares, i + dx[a], j + dy[a], moveHistory);
   }
 };
-
 
 /// TODO:
 // 1. Sa se opreasca DFS la target
